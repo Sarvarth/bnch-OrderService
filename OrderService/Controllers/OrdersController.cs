2e8be7ebdcbc3c88b41d17e16af602380e1f1415
Sarvarth Monga <sarvarth.monga@onsolve.com>
2020-08-03 01:29:43 +0530

    Add project files.

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace OrderService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        // GET orders/5
+        [HttpGet("{id}")]
+        public ActionResult<List<Order>> Get(int id)
+        {
+            return new List<Order> {
+                new Order {
+                    OrderId = 1,
+                    OrderAmount = 100,
+                    OrderDate = Convert.ToDateTime("10-10-2020")
+                },
+                new Order
+                {
+                    OrderId = 2,
+                    OrderAmount = 200,
+                    OrderDate = Convert.ToDateTime("10/11/2019")
+                }};
+        }
+    }
+}
